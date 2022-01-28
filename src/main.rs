ef5a8c70b341f7ed4b24287c38504ec8b994a85e
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-28 12:34:12 +0900

    overwrite

--- src/main.rs
+++ src/main.rs
@@ -14,43 +14,19 @@ extern crate ncurses;
 
 use hekopanda_cli_tools::slot::Slot;
 use ncurses::*;
+use std::io::{stdout, Write};
 
 fn main() {
-    // /* Setup ncurses. */
-    initscr();
-    raw();
-
-    // /* Allow for extended keyboard (like F1). */
-    // keypad(stdscr(), true);
-    // noecho();
-
-    // /* Invisible cursor. */
-    // curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
-
-    // /* Status/help info. */
-    addstr("Let's start slot\n");
-    // addstr("Use the arrow keys to move");
-    refresh(); // 文字出力に必要
     let mut slot = Slot::new();
     loop {
-        // getch();
-        // addstr(&c.to_string());
         slot.spin();
         if slot.is_finish() {
             break;
         }
-        insstr(
-            format!(
-                "{}{}{}",
-                slot.output.0.num, slot.output.1.num, slot.output.2.num
-            )
-            .as_str(),
+        stdout().flush();
+        print!(
+            "\r{}{}{}",
+            slot.output.0.num, slot.output.1.num, slot.output.2.num
         );
-        mv(0, 3);
-        clrtoeol();
-        mv(0, 0);
-        refresh(); // 文字出
-                   // c += 1;
     }
-    endwin();
 }
