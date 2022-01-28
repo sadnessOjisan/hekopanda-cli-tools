22ff933314c96bf513d5e73434803b9166fa24aa
Yuta Ide <sadness.ojisan@gmail.com>
2022-01-28 20:20:40 +0900

    CLI としての体裁を整える (#4)
    * use clap
    
    * update readme

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use hekopanda_cli_tools::slot::Slot;
+use clap::Parser;
+use hekopanda::slot::Slot;
 use std::{io::stdin, sync::mpsc::channel, thread::spawn};
 
-fn main() {
-    let mut slot = Slot::new();
-    let (tx, rx) = channel::<bool>();
-    spawn(move || loop {
-        let received = rx.try_recv();
-        slot.spin();
-        match received {
-            Ok(should_stop) => {
-                if should_stop {
-                    slot.stop();
-                }
-                if slot.is_finish() {
-                    break;
-                }
-            }
-            Err(_) => {}
-        }
-        print!(
-            "\r{}{}{}🐼",
-            slot.output.0.num, slot.output.1.num, slot.output.2.num
-        );
-    });
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+struct Cli {
+    /// Number of times to greet
+    #[clap(short, long)]
+    slot: bool,
+}
 
-    loop {
-        let mut guess = String::new();
-        stdin().read_line(&mut guess).expect("Failed to read line.");
-        let _ = tx.send(true);
+fn main() {
+    let args = Cli::parse();
+    if args.slot {
+        let mut slot = Slot::new();
+        let (tx, rx) = channel::<bool>();
+        spawn(move || loop {
+            let received = rx.try_recv();
+            slot.spin();
+            match received {
+                Ok(should_stop) => {
+                    if should_stop {
+                        slot.stop();
+                    }
+                    if slot.is_finish() {
+                        break;
+                    }
+                }
+                Err(_) => {}
+            }
+            print!(
+                "\r{}{}{}🐼",
+                slot.output.0.num, slot.output.1.num, slot.output.2.num
+            );
+        });
+
+        loop {
+            let mut guess = String::new();
+            stdin().read_line(&mut guess).expect("Failed to read line.");
+            let _ = tx.send(true);
+        }
     }
 }
