c3a830285c23f1b2f4483ed345e75056aa146709
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-30 04:32:39 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
-use hekopanda::{gravure::print, slot::Slot};
-use std::{io::stdin, sync::mpsc::channel, thread::spawn};
+use hekopanda::{gravure::print, slot::do_slot};
 
 #[derive(Parser)]
 #[clap(version, about, long_about = None)]
@@ -16,33 +15,7 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
     if args.slot {
-        let mut slot = Slot::new();
-        let (tx, rx) = channel::<bool>();
-        spawn(move || loop {
-            let received = rx.try_recv();
-            slot.spin();
-            match received {
-                Ok(should_stop) => {
-                    if should_stop {
-                        slot.stop();
-                    }
-                    if slot.is_finish() {
-                        break;
-                    }
-                }
-                Err(_) => {}
-            }
-            print!(
-                "\r{}{}{}🐼",
-                slot.output.0.num, slot.output.1.num, slot.output.2.num
-            );
-        });
-
-        loop {
-            let mut guess = String::new();
-            stdin().read_line(&mut guess).expect("Failed to read line.");
-            let _ = tx.send(true);
-        }
+        do_slot();
     } else if args.gravure {
         print();
     }
--- src/slot.rs
+++ src/slot.rs
@@ -1,7 +1,39 @@
+use std::{io::stdin, sync::mpsc::channel, thread::spawn};
+
 #[derive(Debug)]
 pub struct NumState {
     pub num: u8,
     is_stoped: bool,
+}
+
+pub fn do_slot() {
+    let mut slot = Slot::new();
+    let (tx, rx) = channel::<bool>();
+    spawn(move || loop {
+        let received = rx.try_recv();
+        slot.spin();
+        match received {
+            Ok(should_stop) => {
+                if should_stop {
+                    slot.stop();
+                }
+                if slot.is_finish() {
+                    break;
+                }
+            }
+            Err(_) => {}
+        }
+        print!(
+            "\r{}{}{}🐼",
+            slot.output.0.num, slot.output.1.num, slot.output.2.num
+        );
+    });
+
+    loop {
+        let mut guess = String::new();
+        stdin().read_line(&mut guess).expect("Failed to read line.");
+        let _ = tx.send(true);
+    }
 }
 
 #[derive(Debug)]
