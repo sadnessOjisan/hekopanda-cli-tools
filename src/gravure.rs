916c5872f82dd45d885407e963243e7ba15218c3
sadness_ojisan <sadness.ojisan@gmail.com>
2022-01-30 04:44:59 +0900

    fix unused code

--- src/gravure.rs
+++ src/gravure.rs
@@ -2,11 +2,6 @@ extern crate ncurses;
 use ncurses::*;
 
 pub fn print() {
-    /* If your locale env is unicode, you should use `setlocale`. */
-    // let locale_conf = LcCategory::all;
-    // setlocale(locale_conf, ''zh_CN.UTF-8''); // if your locale is like mine(zh_CN.UTF-8).
-
-    /* Start ncurses. */
     initscr();
 
     let aa = "    ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppWWXkWWkKpWpppppppppWpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
@@ -113,18 +108,8 @@ pub fn print() {
     pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppWMMM#bppppppppppppppppppppppppppppppbpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
     ppbpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppdMMWpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppb";
 
-    /* Print to the back buffer. */
     addstr(aa);
-
-    /* Print some unicode(Chinese) string. */
-    // addstr(''Great Firewall dislike VPN protocol./nGFW 不喜欢 VPN 协议。'');
-
-    /* Update the screen. */
     refresh();
-
-    /* Wait for a key press. */
     getch();
-
-    /* Terminate ncurses. */
     endwin();
 }
